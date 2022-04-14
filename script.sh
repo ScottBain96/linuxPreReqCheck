20edab9fe8ec3e683884b5fb4a06dcbb15d39927
scottbain96 <scott.bain.96@gmail.com>
2022-04-14 22:31:10 +0100

    first comit

---
+++ script.sh
@@ -0,0 +1,62 @@
+
+
+#!/bin/bash
+
+echo "Pre-requisites enabler"
+
+user=$(whoami)
+
+
+echo "checking for net-tools package"
+
+
+
+
+#Function that will check the installed package Net-tools. Using a function to re-check if the install was successfull.
+
+function checkPackageNetTools () {    
+
+
+checkNetPkgs=$(sudo dpkg-query -l | grep -c "net-tools")
+
+local  __resultvar=$1
+
+if [ "$checkNetPkgs" = "1" ];
+
+        then
+               local myresult='1'
+                echo "Net-tools package is confirmed installed"
+
+        else
+
+               local myresult='0'
+               echo "Net-Tools is not installed"
+
+
+fi
+
+    eval $__resultvar="'$myresult'"
+}
+
+
+checkPackageNetTools Result
+
+
+
+if [ "$Result" = "1" ];
+
+then
+
+	echo "Net-tools package is already installed, skipping..."
+
+else
+	echo "Proceeding with get-update and installation."
+	sleep 2
+	sudo apt-get update
+	sudo apt-get install net-tools
+	echo "##### RESULTS #####"
+	checkPackageNetTools Result
+
+fi
+
+

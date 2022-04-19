64cae4c6c909a11e1f40db6aa08a7279f3102535
scottbain96 <scott.bain.96@gmail.com>
2022-04-19 18:36:14 +0100

    added some initial python and sudo command checks

--- script.sh
+++ script.sh
@@ -6,12 +6,66 @@ echo "Pre-requisites enabler"
 #user=$(whoami)
 
 
+
+##TO ADD CHECKER FOR DETARMINING PYTHON 2 or PYTHON3 (different commands are used for each calling each version)
+
+python3PathCheck=$(which python3)
+
+
+if [ "$python3PathCheck" = "/usr/bin/python3" ];
+
+then
+
+echo "python path is correct"
+
+
+else echo "python default path does not seem correct, paths found: "
+
+which -a python3 
+
+fi
+
+
+
+#might need to add a way to reset the terminal to avoid false positives for sudo no credentials requested
+#example, if they had already typed the sudo creds for a different command under the same session, they will
+#most likely be cached.
+
+
+folderName="test"$(date +"%T")
+
+echo "Testing folder creation with sudo, should not request any credentials"
+
+sudo mkdir /opt/"$folderName"
+
+
+if [ -d "/opt/""$folderName" ];
+
+then
+
+echo "Folder was created successfuly"
+
+else
+
+echo "Folder does not seem to exist. Please doublecheck /opt/ path"
+
+fi
+
+
+
+
+
+
+
+
+
 echo "checking for net-tools package"
+
+
 
 #Function that will check the installed package Net-tools. Using a function to re-check if the install was successfull.
 
 function checkPackageNetTools () {    
-
 
 checkNetPkgs=$(sudo dpkg-query -l | grep -c "net-tools")
 
@@ -48,7 +102,7 @@ then
 
 else
 	echo "Proceeding with get-update and installation."
-	sleep 2
+	#sleep 2
 	sudo apt-get update
 	sudo apt-get install net-tools
 	echo "##### RESULTS #####"

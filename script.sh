643f85abb9824f56111714ba133fd4513bae00c1
scottbain96 <scott.bain.96@gmail.com>
2022-04-19 19:48:47 +0100

    Added output to log and some extra checks (Linux version, user checks, command checks).

--- script.sh
+++ script.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-echo "Pre-requisites enabler"
+echo "Pre-requisites cheker for Linux / MacOS"
 
-#for later,
-#user=$(whoami)
+#log file for results
+
+
+logFileName="logPreReq-"$(date +"%T")".txt"
+
+#check user logged
+userLogged=$(whoami)
+
+
+echo "logged in as user:  $userLogged" | tee $logFileName
+
+
+#displaying the Linux release
+
+
+cat /etc/*release | tee -a $logFileName
+
+
+#To check for a list of commands that the user is allowed to run:
+
+
+sudo -l | tee -a $logFileName 
 
 
 
@@ -16,12 +36,12 @@ if [ "$python3PathCheck" = "/usr/bin/python3" ];
 
 then
 
-echo "python path is correct"
+echo "python path is correct: $python3PathCheck" | tee -a $logFileName 
 
 
-else echo "python default path does not seem correct, paths found: "
+else echo "python default path does not seem correct, paths found: " | tee -a $logFileName 
 
-which -a python3 
+which -a python3 | tee -a $logFileName
 
 fi
 
@@ -34,20 +54,20 @@ fi
 
 folderName="testDir"$(date +"%T")
 
-echo "Testing folder creation with sudo, should not request any credentials"
+echo "Testing folder creation with sudo, should not request any credentials" | tee -a $logFileName 
 
-sudo mkdir /opt/"$folderName"
+sudo mkdir /opt/"$folderName" | tee -a $logFileName
 
 
 if [ -d "/opt/""$folderName" ];
 
 then
 
-echo "Folder was created successfuly"
+echo "Folder was created successfuly" | tee -a $logFileName 
 
 else
 
-echo "Folder does not seem to exist. Please doublecheck /opt/ path"
+echo "Folder does not seem to exist. Please doublecheck /opt/ path" | tee -a $logFileName
 
 fi
 
@@ -59,13 +79,13 @@ fi
 
 
 
-echo "checking for net-tools package"
-
+echo "checking for net-tools package" | tee -a $logFileName
+ 
 
 
 #Function that will check the installed package Net-tools. Using a function to re-check if the install was successfull.
 
-function checkPackageNetTools () {    
+function checkPackageNetTools () {
 
 checkNetPkgs=$(sudo dpkg-query -l | grep -c "net-tools")
 
@@ -75,12 +95,12 @@ if [ "$checkNetPkgs" = "1" ];
 
         then
                local myresult='1'
-                echo "Net-tools package is confirmed installed"
+                echo "Net-tools package is confirmed installed" | tee -a $logFileName
 
         else
 
                local myresult='0'
-               echo "Net-Tools is not installed"
+               echo "Net-Tools is not installed" | tee -a $logFileName
 
 
 fi
@@ -98,16 +118,20 @@ if [ "$Result" = "1" ];
 
 then
 
-	echo "Net-tools package is already installed, skipping..."
+	echo "Net-tools package is already installed, skipping..." | tee -a $logFileName
 
 else
-	echo "Proceeding with get-update and installation."
+	echo "Proceeding with get-update and installation." | tee -a $logFileName
 	#sleep 2
-	sudo apt-get update
-	sudo apt-get install net-tools
+	sudo apt-get update | tee -a $logFileName
+	sudo apt-get install net-tools | tee -a $logFileName
 	echo "##### RESULTS #####"
 	checkPackageNetTools Result
 
 fi
 
 
+#END OF SCRIPT CONFIRMATION
+
+
+echo "Script ended, please provide results to support" | tee -a $logFileName
